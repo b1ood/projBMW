1373d22304ee630565e8518e2165b51a3166e2e8
b1ood <71218071+b1ood@users.noreply.github.com>
2022-04-18 09:49:49 +0300

    Start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sstep';
+  title = 'step';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RouterModule } from "@angular/router";
+import { SliderMainComponent } from './slider-main/slider-main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+    declarations: [
+        AppComponent,
+        MainComponent,
+        SliderMainComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: MainComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
