8ea0ddf3a42e8bfa8ae63fd4a08af20d39281ea9
b1ood <71218071+b1ood@users.noreply.github.com>
2022-08-23 17:45:59 +0300

    First commit

---
+++ src/app/models/models.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-models',
+  templateUrl: './models.component.html',
+  styleUrls: ['./models.component.scss']
+})
+export class ModelsComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void { }
+
+
+
+
+}
